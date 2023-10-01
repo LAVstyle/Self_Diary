fa9de353f63c411fe7f211d62bdceb98a98954a1
LAVstyle <luchin250398@yandex.ru>
2023-10-01 16:09:26 +0700

    event_listener

--- scripts.js
+++ scripts.js
@@ -1,8 +1,26 @@
-console.log("Everything is OK")
-var num1= 5;
-var num1 = 7;
-let num2 = 6;
+// Мое название документа
+const buttonElement = document.getElementById("profile");
+const squareDiv = document.getElementById("main");
 
+// чтобы реагировать на нажатие кнопки, записываем функцию в свойство onclick.
+// Эта функция будет вызываться при каждом нажатии на кнопку. Часто говорят,
+// что эта функция обрабатывает событие
+buttonElement.onclick = function () {
+  squareDiv.style = `color: ${getColor()};`;
+};
 
-console.log(num1, num2)
-
+function getColor() {
+  const colors = [
+    "#49A16C",
+    "#064236",
+    "#ED6742",
+    "#F498AD",
+    "#1A5AD7",
+    "#AFC9DA",
+    "#FFD829",
+    "#282A2E",
+    "#5E6064",
+    "#E6E6E6",
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
